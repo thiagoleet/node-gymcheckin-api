fe5fb4772ec3a62e63f78478b04ed63072f65fd5
Thiago Leite <thiagofmleite@gmail.com>
2024-06-24 09:35:24 -0300

    Added middleware to validate JWT

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,19 @@
+import { makeGetUserProfileUseCase } from "@/factories/make-get-user-profile-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify();
-
   const { sub } = request.user;
 
-  return reply.status(200).send({ message: "User profile" });
+  const getUserProfile = makeGetUserProfileUseCase();
+
+  const { user } = await getUserProfile.execute({ userId: sub });
+
+  // TODO: Prepare a response type
+  return reply.status(200).send({
+    message: "User profile",
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  });
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,10 +1,17 @@
 import { FastifyInstance } from "fastify";
 import { register, authenticate, profile } from "@/http/controllers";
+import { verifyJwt } from "./middlewares";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
 
   // Authenticated
-  app.get("/me", profile);
+  app.get(
+    "/me",
+    {
+      onRequest: [verifyJwt],
+    },
+    profile
+  );
 }
